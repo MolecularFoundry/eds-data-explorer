da4f5c7abe92432669e6a59c28092e013d672d37
Catherine Tan <catherine95118@gmail.com>
2025-10-17 15:35:08 -0700

    shorten server proxy logic

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -10,11 +10,7 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-        secure: false,
-      },
+      '/api': 'http://localhost:8000',
     },
   },
 })
